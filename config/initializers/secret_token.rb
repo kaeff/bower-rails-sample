7bd77b0e35bcaaa5970e9835486594d2b079a4fe
Klaus Fleerkötter <kaeff@gmx.net>
2012-12-23 15:59:00 +0100

    rails new rails_bower

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBower::Application.config.secret_token = '22b7ae0773a46b289a71ca0a4d583af198b37f8f4030f8c2e70aeffe58a326dcaa4b250c60d83e5f34e10e8e2e8bc556f1153174f6856173cc2e0fd4ce4804e0'
