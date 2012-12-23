6c6bbc9f698be0e8834c2aa32152c6842d08d4f3
Klaus Fleerkötter <kaeff@gmx.net>
2012-12-24 00:25:57 +0100

    Adds Bower sample

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+RailsBower::Application.configure do
+ config.assets.append_path 'components'
+end
+
 # Initialize the rails application
 RailsBower::Application.initialize!
+
